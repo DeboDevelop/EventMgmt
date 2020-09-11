0992028bbe3888d49994127c6b249da352fcff06
DeboDevelop <debajyotidofficial@gmail.com>
2020-09-11 19:05:59 +0530

    Wrote some unit test for Event

--- src/server.js
+++ src/server.js
@@ -34,3 +34,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`The Server is running on http://localhost:${PORT}`)
 );
+
+module.exports = app;
---
+++ src/test/test.js
@@ -0,0 +1,50 @@
+let mongoose = require("mongoose");
+let Event = require("../Models/event");
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Events", () => {
+  beforeEach((done) => {
+    Event.deleteMany({}, (err) => {
+      done();
+    });
+  });
+  describe("/GET Event", () => {
+    it("it should GET all the events", (done) => {
+      chai
+        .request(server)
+        .get("/api/event")
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+  /*
+   * Test the /POST route
+   */
+  describe("/POST event", () => {
+    it("it should POST a event", (done) => {
+      let myEvent = {
+        event_name: "Wake up",
+        user: "test",
+        description: "Wake me up",
+        date_and_time: new Date().getTime(),
+      };
+      chai
+        .request(server)
+        .post("/api/event")
+        .send(myEvent)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+});
