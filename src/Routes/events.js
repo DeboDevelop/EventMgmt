fb4bbdcfdf462a09096ce9301a2e997a9f301f54
DeboDevelop <debajyotidofficial@gmail.com>
2020-09-11 17:36:44 +0530

    Created a update route and delete route for event.

--- src/Routes/events.js
+++ src/Routes/events.js
@@ -36,4 +36,36 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Updating One
+router.patch("/:id", getEvent, async (req, res) => {
+  if (req.body.event_name != null) {
+    res.event.event_name = req.body.event_name;
+  }
+  if (req.body.user != null) {
+    res.event.user = req.body.user;
+  }
+  if (req.body.description != null) {
+    res.event.description = req.body.description;
+  }
+  if (req.body.date_and_time != null) {
+    res.event.date_and_time = req.body.date_and_time;
+  }
+  try {
+    const updatedEvent = await res.event.save();
+    res.json(updatedEvent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Deleting One
+router.delete("/:id", getEvent, async (req, res) => {
+  try {
+    await res.event.remove();
+    res.json({ message: "Event Deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
